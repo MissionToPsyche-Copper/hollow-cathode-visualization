02cc590db3290973e22c2a22e2a386702979a2db
JackBlicha <57300971+JackBlicha@users.noreply.github.com>
2021-11-09 18:52:28 -0500

    initial commit with some reactjs exploration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Welcome(props) {
+  switch (props.name) {
+    case "Jake": return <h1>Hello {props.name} Smith!</h1>;
+    case "Jack": return <h1>Hello {props.name} Blicha!</h1>;
+    case "Huy": return <h1>Hello {props.name} Tran!</h1>;
+    default: return <h1>Hello {props.name}!</h1>;
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Welcome name={"Jack"}/>
+      <Welcome name={"Jake"}/>
+      <Welcome name={"World"}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
