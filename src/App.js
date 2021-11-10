19c4cdd149904970dd7882944a1f24f794b45856
JackBlicha <57300971+JackBlicha@users.noreply.github.com>
2021-11-09 21:23:58 -0500

    Footers are irritating, android studio is a blessing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
+// primary coding style:
 function Welcome(props) {
   switch (props.name) {
     case "Jake": return <h1>Hello {props.name} Smith!</h1>;
@@ -9,27 +11,37 @@ function Welcome(props) {
     default: return <h1>Hello {props.name}!</h1>;
   }
 }
+// // alternative: ES6 class
+// class Welcome extends React.Component {
+//     render() {
+//         switch (this.props.name) {
+//             case "Jake": return <h1>Hello {this.props.name} Smith!</h1>;
+//             case "Jack": return <h1>Hello {this.props.name} Blicha!</h1>;
+//             case "Huy": return <h1>Hello {this.props.name} Tran!</h1>;
+//             default: return <h1>Hello {this.props.name}!</h1>;
+//         }
+//     }
+// }
 
 function App() {
   return (
     <div className="App">
-      <Welcome name={"Jack"}/>
-      <Welcome name={"Jake"}/>
-      <Welcome name={"World"}/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <Welcome name={"Jack"}/>
+            <Welcome name={"World"}/>
+            <Welcome name={"Jake"}/>
+            <img src={logo} className="App-logo" alt="logo" />
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                Learn React
+            </a>
+            <p>testing paragraph</p>
+            <ul>
+                <li>testing list items</li>
+                <li>testing list items</li>
+                <li>testing list items</li>
+                <li>testing list items</li>
+            </ul>
+        </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,33 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
-// a
+
+// // basic sample
+// const element = <Welcome name="Jake" />;
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// );
+
+let tickvar = 0;
+function tick() {
+    const element = (
+        <div>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            <p id={"tick_paragraph"}>{tickvar}</p>
+            {tickvar = tickvar+1}
+            <p> a </p>
+            <p> a </p>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+// setInterval(tick, 1000);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
