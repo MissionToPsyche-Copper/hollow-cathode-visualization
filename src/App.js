37d72bccaa6361984ccc7e79324dbfea9dcb3417
JackBlicha <57300971+JackBlicha@users.noreply.github.com>
2021-11-11 17:44:51 -0500

    Canvas element is in there now

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-// primary coding style:
-function Welcome(props) {
-  switch (props.name) {
-    case "Jake": return <h1>Hello {props.name} Smith!</h1>;
-    case "Jack": return <h1>Hello {props.name} Blicha!</h1>;
-    case "Huy": return <h1>Hello {props.name} Tran!</h1>;
-    default: return <h1>Hello {props.name}!</h1>;
-  }
-}
+// // primary coding style:
+// function Welcome(props) {
+//   switch (props.name) {
+//     case "Jake": return <h1>Hello {props.name} Smith!</h1>;
+//     case "Jack": return <h1>Hello {props.name} Blicha!</h1>;
+//     case "Huy": return <h1>Hello {props.name} Tran!</h1>;
+//     default: return <h1>Hello {props.name}!</h1>;
+//   }
+// }
 // // alternative: ES6 class
 // class Welcome extends React.Component {
 //     render() {
@@ -27,23 +27,81 @@ function App() {
   return (
     <div className="App">
         <header className="App-header">
-            <Welcome name={"Jack"}/>
-            <Welcome name={"World"}/>
-            <Welcome name={"Jake"}/>
-            <img src={logo} className="App-logo" alt="logo" />
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                Learn React
-            </a>
-            <p>testing paragraph</p>
-            <ul>
-                <li>testing list items</li>
-                <li>testing list items</li>
-                <li>testing list items</li>
-                <li>testing list items</li>
-            </ul>
+            {/*<Welcome name={"Jack"}/>*/}
+            {/*<Welcome name={"World"}/>*/}
+            {/*<Welcome name={"Jake"}/>*/}
+            {/*<img src={logo} className="App-logo" alt="logo" />*/}
+            <CanvasExample/>
+            {/*<LandingPage/>*/}
+            <LoggingButton/>
         </header>
     </div>
   );
 }
 
 export default App;
+
+class LandingPage extends React.Component {
+    handleClick() {
+        console.log('You clicked the button, *this* button:', this);
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={() => this.handleClick()}>
+                    Click me
+                </button>
+            </div>
+        );
+    }
+}
+
+class LoggingButton extends React.Component {
+    handleClick() {
+        console.log('You clicked the button, *this* button:', this);
+    }
+
+    render() {
+        // This syntax ensures `this` is bound within handleClick // (comment from online guide, idk what he is talking about)
+        return (
+            <button id={"myButton"} onClick={() => this.handleClick()}>
+                Click me
+            </button>
+        );
+    }
+}
+
+// this element has it's own canvas element stored
+class CanvasExample extends React.Component {
+    constructor(){
+        console.log("CanvasExample.constructor")
+        super()
+        this.canvas = React.createRef()                              //// 1 - create ref
+    }
+
+    componentDidMount() {
+        console.log("CanvasExample.componentDidMount")
+        this.baseDrawing()
+    }
+
+    baseDrawing(){
+        console.log("CanvasExample.baseDrawing")
+        const ctx = this.canvas.current.getContext('2d')     //// 3 - access node using .current
+
+        var myLogo = new Image();
+        myLogo.src = logo;
+        ctx.drawImage(myLogo, -60, -60); //draw logo
+
+        ctx.fillRect(300, 400, 200, 200)
+    }
+
+    render(){
+        console.log("CanvasExample.render")
+        return (
+            <div>
+                <canvas ref={this.canvas} width={1600} height={750}> You need a better browser :( </canvas>      {/*//// 2 - attach ref to node}*/}
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logo from "./logo.svg";
 
 ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
+
+// class Toggle extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {isToggleOn: true};
+//
+//         // This binding is necessary to make `this` work in the callback
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+//
+//     handleClick() {
+//         this.setState(prevState => ({
+//             isToggleOn: !prevState.isToggleOn
+//         }));
+//     }
+//
+//     render() {
+//         return (
+//             <button onClick={this.handleClick}>
+//                 {this.state.isToggleOn ? 'ON' : 'OFF'}
+//             </button>
+//         );
+//     }
+// }
+//
+// ReactDOM.render(
+//     <Toggle />,
+//     document.getElementById('myButton')
+// );
 
 // // basic sample
 // const element = <Welcome name="Jake" />;
@@ -16,19 +46,17 @@ ReactDOM.render(
 //     document.getElementById('root')
 // );
 
-let tickvar = 0;
-function tick() {
-    const element = (
-        <div>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-            <p id={"tick_paragraph"}>{tickvar}</p>
-            {tickvar = tickvar+1}
-            <p> a </p>
-            <p> a </p>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-}
+// let tickvar = 0;
+// function tick() {
+//     const element = (
+//         <div>
+//             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//             <p id={"tick_paragraph"}>{tickvar}</p>
+//             {tickvar = tickvar+1}
+//         </div>
+//     );
+//     ReactDOM.render(element, document.getElementById('root'));
+// }
 
 // setInterval(tick, 1000);
 
