314b31fd147a40bd49d6d5fba37165783877b82f
JackBlicha <57300971+JackBlicha@users.noreply.github.com>
2021-11-11 18:54:26 -0500

    When you click the canvas the next box appears

--- src/App.js
+++ src/App.js
@@ -77,30 +77,64 @@ class CanvasExample extends React.Component {
     constructor(){
         console.log("CanvasExample.constructor")
         super()
+        this.currentStage = 0;
         this.canvas = React.createRef()                              //// 1 - create ref
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
         console.log("CanvasExample.componentDidMount")
-        this.baseDrawing()
+        this.draw_baseDrawing()
     }
 
-    baseDrawing(){
-        console.log("CanvasExample.baseDrawing")
+    draw_baseDrawing(){
+        console.log("CanvasExample.draw_baseDrawing")
+        this.currentStage = 1;
         const ctx = this.canvas.current.getContext('2d')     //// 3 - access node using .current
 
+        ctx.fillStyle = 'rgba(255,255,255,0.4)';
+        ctx.fillRect(200, 400, 200, 200)
         var myLogo = new Image();
         myLogo.src = logo;
         ctx.drawImage(myLogo, -60, -60); //draw logo
-
-        ctx.fillRect(300, 400, 200, 200)
+        ctx.save();
     }
+
+    handleClick() {
+        console.log("CanvasExample.handleClick")
+        switch (this.currentStage) {
+            // case 2: this.draw_csv...();
+            case 2: this.draw_csv2(); break;
+            case 1: this.draw_csv1(); break;
+            case 0: this.draw_baseDrawing(); break;
+            default: console.error("currentStage === weird"); break;
+        }
+    }
+
+    draw_csv1(){
+        console.log("CanvasExample.draw_csv1")
+        this.currentStage = 2;
+        const ctx = this.canvas.current.getContext('2d');
+
+        ctx.fillStyle = 'rgba(63,63,63,0.4)';
+        ctx.fillRect(300, 400, 200, 200);
+    }
+
+    draw_csv2(){
+        this.currentStage = 3;
+        console.log("CanvasExample.draw_csv2");
+        const ctx = this.canvas.current.getContext('2d');
+
+        ctx.fillStyle = 'rgba(83,220,183,0.4)';
+        ctx.fillRect(400, 400, 200, 200);
+    }
+
 
     render(){
         console.log("CanvasExample.render")
         return (
             <div>
-                <canvas ref={this.canvas} width={1600} height={750}> You need a better browser :( </canvas>      {/*//// 2 - attach ref to node}*/}
+                <canvas ref={this.canvas} width={1600} height={750} onClick={this.handleClick}> You need a better browser :( </canvas>      {/*//// 2 - attach ref to node}*/}
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -60,6 +60,32 @@ ReactDOM.render(
 
 // setInterval(tick, 1000);
 
+class Text extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
