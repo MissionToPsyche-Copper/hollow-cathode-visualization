47976fcfa486318a7b9a46b6b85349228df80bea
JackBlicha <57300971+JackBlicha@users.noreply.github.com>
2021-11-22 16:30:59 -0500

    Code cleaning

--- src/App.js
+++ src/App.js
@@ -26,40 +26,27 @@ class CanvasExampleWithButton extends React.Component {
 
     constructor(props){
         super() // I don't understand what this line does - Jack
+        console.log("CanvasExample.constructor() called")
 
         // initialize canvas instance variables
         this.canvas = React.createRef()                              //// 1 - create ref
-        console.log("CanvasExample.constructor()")
 
         // bind handler function(s)
         this.HeatInsertToggle_HandleClick = this.HeatInsertToggle_HandleClick.bind(this);
         this.GasFeedToggle_HandleClick = this.GasFeedToggle_HandleClick.bind(this);
         this.KeeperElectrodeToggle_HandleClick = this.KeeperElectrodeToggle_HandleClick.bind(this);
-        // this.draw_csv_Heat_Insert = this.draw_csv_Heat_Insert.bind(this);
-        // this.draw_csv_gas_feed = this.draw_csv_gas_feed.bind(this);
 
         // initialize instance variables
-        // this.deltastage = props.deltastage;
-        // this.scene = props.scene;
         this.state = { deltastage: props.deltastage, scene: props.scene };
 
-        console.log("this.state.scene", this.state.scene)
-        console.log("this.deltastage", this.deltastage)
 
 
-
-
-
-
-        // this.deltastage = 1; //delta stage
-        // this.scene = [true, false, false, false];
-        // this.scene = [base, inserts, gas, keeper];
-
+        console.log("   constructor:: this.state.scene", this.state.scene)
+        console.log("   constructor:: this.deltastage", this.deltastage)
 
         // misc items for testing and learning
         this.moon = new Image();
         this.moon.src = 'moon.png';
-        // this.firstRun = true;
     }
 
     /**
@@ -67,11 +54,10 @@ class CanvasExampleWithButton extends React.Component {
      * Called when canvas element is mounted on page (canvas element is unusable up until this point)
      */
     componentDidMount() {
-        console.log("CanvasExample.componentDidMount()")
-        // this.draw_baseDrawing()
+        console.log("CanvasExample.componentDidMount() called")
 
-        console.log("componentDidMount:: this.state.deltastage", this.state.deltastage);
-        console.log("componentDidMount:: this.state.scene", this.state.scene);
+        console.log("   componentDidMount:: this.state.deltastage", this.state.deltastage);
+        console.log("   componentDidMount:: this.state.scene", this.state.scene);
 
         this.scenarioRefresh();
     }
@@ -79,13 +65,13 @@ class CanvasExampleWithButton extends React.Component {
     /**
      * scenarioRefresh()
      *
-     * does what we wish the constructor could do
+     * Populates the canvas based on the current state, is hopefully called whenever a change is made (ex: the onClick functions)
      */
     scenarioRefresh() {
-        console.log("CanvasExample.scenarioRefresh()")
+        console.log("CanvasExample.scenarioRefresh() called")
 
-        console.log("scenarioRefresh:: this.state.deltastage", this.state.deltastage);
-        console.log("scenarioRefresh:: this.state.scene", this.state.scene);
+        console.log("   scenarioRefresh:: this.state.deltastage", this.state.deltastage);
+        console.log("   scenarioRefresh:: this.state.scene", this.state.scene);
 
         //do logic based on deltastage and scene
         if(this.state.scene[0] === true && (this.state.deltastage === 0 || this.state.deltastage === undefined)){
@@ -104,9 +90,7 @@ class CanvasExampleWithButton extends React.Component {
             console.log("scenarioRefresh no new scenarios (error?)")
         }
 
-        // this.setState((state, props) => {
-        //     return {deltastage: props.deltastage, scene: [props.scene[0], props.scene[1], props.scene[2], props.scene[3]]};
-        // });
+        // console.log("-------------------------------------------scenarioRefresh (end)-------------------------------------------------------")
     }
 
     /**
@@ -114,7 +98,7 @@ class CanvasExampleWithButton extends React.Component {
      * Onclick handler for the heat insert toggle button
      */
     HeatInsertToggle_HandleClick() {
-        console.log("CanvasExample.HeatInsertToggle_HandleClick")
+        console.log("CanvasExample.HeatInsertToggle_HandleClick()")
         // let heatInsertButton = document.getElementById("HeatInsertToggle");
         // if(this.scene[1] === true){
         //     heatInsertButton.classList.add("active")
@@ -123,33 +107,13 @@ class CanvasExampleWithButton extends React.Component {
         //     heatInsertButton.classList.remove("active")
         // }
 
-        // switch (this.deltastage) {
-        //     // case #: this.draw_csv...();
-        //     case 4: this.scene[this.deltastage] = true; this.deltastage = 0; this.clearCanvas(); break; // (stop the moon)
-        //     case 3: this.scene[this.deltastage] = true; this.deltastage = 4; cancelAnimationFrame(this.myReq); break; // (stop the moon)
-        //     case 2: this.scene[this.deltastage] = true; this.deltastage = 3; this.draw_csv_gas_feed(); break; // (start the moon)
-        //     case 1: this.scene[this.deltastage] = true; this.deltastage = 2; this.draw_csv_Heat_Insert(); break;
-        //     case 0: this.scene[this.deltastage] = true; this.deltastage = 1; this.draw_baseDrawing(); break;
-        //     default: console.error("deltastage === weird"); break;
-        // }
-
-        // this.scene[1] = !this.scene[1];
-        // this.scene[1] = true;
-        // this.deltastage = 1;
 
         // change the current state, scenarioRefresh() as a callback ensures the system synchronously calls scenarioRefresh AFTER the state has been updated
         this.setState((state, props) => {
             return { deltastage: 1, scene: [state.scene[0], !state.scene[1], state.scene[2], state.scene[3]] };
         }, () => {this.scenarioRefresh()});
 
-        // WARNING: code here is not guaranteed to be synchronously executed
-
-
-        // this.render();
-        // ReactDOM.render(
-        //     <CanvasExampleWithButton id={"CanvasExampleWithButton"} deltastage={this.deltastage} scene={this.scene}/>,
-        //     document.getElementById('canvasHolder')
-        // );
+        // WARNING: code past setState will not be synchronously executed
     }
 
     /**
@@ -159,20 +123,12 @@ class CanvasExampleWithButton extends React.Component {
     GasFeedToggle_HandleClick() {
         console.log("CanvasExample.GasFeedToggle_HandleClick")
 
-        // this.scene[2] = !this.scene[2];
-        // this.state.scene[2] = true;
-        // this.deltastage = 2;
-
         // change the current state, scenarioRefresh() as a callback ensures the system synchronously calls scenarioRefresh AFTER the state has been updated
         this.setState((state, props) => {
             return { deltastage: 2, scene: [state.scene[0], state.scene[1], !state.scene[2], state.scene[3]] };
         }, () => {this.scenarioRefresh()});
 
-        // this.render();
-        // ReactDOM.render(
-        //     <CanvasExampleWithButton id={"CanvasExampleWithButton"} deltastage={this.deltastage} scene={this.scene}/>,
-        //     document.getElementById('canvasHolder')
-        // );
+        // WARNING: code past setState will not be synchronously executed
     }
 
     /**
@@ -180,25 +136,14 @@ class CanvasExampleWithButton extends React.Component {
      * Onclick handler for the keeper electrode toggle button
      */
     KeeperElectrodeToggle_HandleClick() {
-        console.log("CanvasExample.KeeperElectrodeToggle_HandleClick")
-
-        // this.scene[3] = !this.scene[3];
-        // this.scene[3] = true;
-        // this.deltastage = 3;
+        console.log("CanvasExample.KeeperElectrodeToggle_HandleClick()")
 
         // change the current state, scenarioRefresh() as a callback ensures the system synchronously calls scenarioRefresh AFTER the state has been updated
         this.setState((state, props) => {
             return { deltastage: 3, scene: [state.scene[0], state.scene[1], state.scene[2], !state.scene[3]] };
         }, () => {this.scenarioRefresh()});
 
-
-        // this.render();
-        // ReactDOM.render(
-        //     <CanvasExampleWithButton id={"CanvasExampleWithButton"} deltastage={this.deltastage} scene={this.scene}/>,
-        //     document.getElementById('canvasHolder')
-        // );
-        // this.forceUpdate();
-        // this.setState(this.state);
+        // WARNING: code past setState will not be synchronously executed
     }
 
     // /**
@@ -206,7 +151,7 @@ class CanvasExampleWithButton extends React.Component {
     //  * Clears all drawings from the canvas.
     //  */
     // clearCanvas(){
-    //     this.scene = [false, false, false, false, false, false]; // reset the scenes
+    //     this.scene = [false, false, false, false]; // reset the scenes
     //     console.log("CanvasExample.clearCanvas")
     //     if(this.scene[this.deltastage] === true){
     //         console.log("CanvasExample.clearCanvas should not be drawn");
@@ -220,10 +165,13 @@ class CanvasExampleWithButton extends React.Component {
     /**
      * draw_baseDrawing()
      * Function to draw the base cathode (currently only draws a gray square)
+     *
      * @returns {null}
      */
     draw_baseDrawing(){
         console.log("CanvasExample.draw_baseDrawing")
+
+        // check if this layer is already drawn
         // if(this.state.scene[this.state.deltastage] === false){
         //     console.log("CanvasExample.draw_baseDrawing should not be drawn")
         //     return null;
@@ -231,6 +179,7 @@ class CanvasExampleWithButton extends React.Component {
 
         const ctx = this.canvas.current.getContext('2d'); //ctx = the canvas element from react
 
+        // draw rectangle
         ctx.fillStyle = 'rgba(255,255,255,0.4)'; //set the pen color
         ctx.fillRect(200, 400, 200, 200) //draw a filled in rectangle
 
@@ -239,14 +188,15 @@ class CanvasExampleWithButton extends React.Component {
         // myLogo.src = logo;
         // ctx.drawImage(myLogo, -60, -60); //draw react logo image
         ctx.save();
+
+        // draw text
         ctx.save();
         ctx.font = "30px Arial";
         ctx.fillStyle = 'rgb(255,255,255)';
         ctx.fillText("Base Drawing", canvas_width/2 + 200, canvas_height/2 + 200);
         ctx.restore();
-        console.log("draw_baseDrawing:: this.state.deltastage", this.state.deltastage);
-        console.log("draw_baseDrawing:: this.state.scene", this.state.scene);
-        console.log("-------------------------------------------draw_baseDrawing-------------------------------------------------------")
+
+        // console.log("-------------------------------------------draw_baseDrawing (end)-------------------------------------------------------")
     }
 
     /**
@@ -256,7 +206,9 @@ class CanvasExampleWithButton extends React.Component {
      * @returns {null}
      */
     draw_csv_Heat_Insert(){
-        // console.log("CanvasExample.draw_csv_Heat_Insert")
+        console.log("draw_csv_Heat_Insert called")
+
+        // check if this layer is already drawn
         // if(this.state.scene[this.state.deltastage] === false){
         //     console.log("CanvasExample.draw_csv_Heat_Insert should not be drawn")
         //     return null;
@@ -264,21 +216,24 @@ class CanvasExampleWithButton extends React.Component {
 
         const ctx = this.canvas.current.getContext('2d');
 
+        // draw rectangle
         ctx.fillStyle = 'rgba(63,63,63,0.4)';
         ctx.fillRect(300, 400, 200, 200);
 
+        // draw text
         ctx.save();
         ctx.font = "30px Arial";
         ctx.fillStyle = 'rgb(255,255,255)';
         ctx.fillText("Heat Insert", canvas_width/2, canvas_height/2 - 30);
         ctx.restore();
-        console.log("draw_csv_Heat_Insert:: this.state.deltastage", this.state.deltastage);
-        console.log("draw_csv_Heat_Insert:: this.state.scene", this.state.scene);
-        console.log("-------------------------------------------draw_csv_Heat_Insert-------------------------------------------------------")
+
+        // console.log("-------------------------------------------draw_csv_Heat_Insert (end)-------------------------------------------------------")
     }
 
     draw_csv_gas_feed(){
-        // console.log("CanvasExample.draw_csv_gas_feed");
+        console.log("draw_csv_gas_feed called");
+
+        // check if this layer is already drawn
         // if(this.scene[this.deltastage] === false){
         //     console.log("CanvasExample.draw_csv_gas_feed should not be drawn")
         //     return null;
@@ -302,6 +257,7 @@ class CanvasExampleWithButton extends React.Component {
         //
         // ctx.restore(); // leave the moon layer so that only the moon layer spins (restore layer (010)
 
+        // draw rectangle
         ctx.fillStyle = 'rgba(31,100,84,0.65)';
         ctx.fillRect(400, 400, 200, 200);
 
@@ -315,13 +271,13 @@ class CanvasExampleWithButton extends React.Component {
         ctx.fillText("Gas Feed", canvas_width/2, canvas_height/2);
         ctx.restore();
 
-        console.log("draw_csv_gas_feed:: this.state.deltastage", this.state.deltastage);
-        console.log("draw_csv_gas_feed:: this.state.scene", this.state.scene);
-        console.log("-------------------------------------------draw_csv_gas_feed-------------------------------------------------------")
+        // console.log("-------------------------------------------draw_csv_gas_feed (end)-------------------------------------------------------")
     }
 
     draw_csv_keeper_electrode(){
-        console.log("CanvasExample.draw_csv_keeper_electrode");
+        console.log("draw_csv_keeper_electrode called");
+
+        // check if this layer is already drawn
         // if(this.scene[this.deltastage] === false){
         //     console.log("CanvasExample.draw_csv_keeper_electrode should not be drawn")
         //     return null;
@@ -345,24 +301,25 @@ class CanvasExampleWithButton extends React.Component {
         //
         // ctx.restore(); // leave the moon layer so that only the moon layer spins (restore layer (010)
 
+        // draw rectangle
         ctx.fillStyle = 'rgba(0,9,7,0.65)';
         ctx.fillRect(500, 400, 200, 200);
 
         // // request the browser draws/runs this whole function
         // this.myReq = window.requestAnimationFrame(this.draw_csv_keeper_electrode);
 
+        // draw text
         ctx.save();
         ctx.font = "30px Arial";
         ctx.fillStyle = 'rgb(255,255,255)';
         ctx.fillText("Keeper Electrode", canvas_width/2, canvas_height/2 + 30);
         ctx.restore();
-        console.log("draw_csv_keeper_electrode:: this.state.deltastage", this.state.deltastage);
-        console.log("draw_csv_keeper_electrode:: this.state.scene", this.state.scene);
-        console.log("-------------------------------------------draw_csv_keeper_electrode-------------------------------------------------------")
+
+        // console.log("-------------------------------------------draw_csv_keeper_electrode (end)-------------------------------------------------------")
     }
 
     render(){
-        console.log("CanvasExample.render")
+        console.log("CanvasExample.render called")
         return (
             <>
                 <canvas id={"canvas"} ref={this.canvas} width={canvas_width} height={canvas_height} deltastage={this.state.deltastage} scene={this.state.scene} > You need a better browser :( </canvas>
