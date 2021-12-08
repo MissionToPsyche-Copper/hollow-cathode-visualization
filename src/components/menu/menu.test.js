c17ac102c46cf337e12bbf9415ee0ad715a9c99d
HuyTran <htt5059@psu.edu>
2021-12-08 14:04:51 -0500

    complete test script skeleton

--- src/components/menu/menu.test.js
+++ src/components/menu/menu.test.js
@@ -1,15 +1,19 @@
-var should = require('chai').should()
+const should = require('chai').should()
 const {promisify} = require('util')
-const {Builder, By, Key} = require("selenium-webdriver");
+const {Builder, By, Key} = require("selenium-webdriver")
 const sleep= promisify(setTimeout)
+//const {Eyes, Target, Configuration, BatchInfo} = require('@applitools/eyes-images')
+//const {config} = require("chai");
 
 describe('Header&FooterTesting', function () {
-    this.timeout(30000)
+    this.timeout(300000)
     let driver
     let vars
     beforeEach(async function() {
         driver = await new Builder().forBrowser('firefox').build()
         vars = {}
+        await driver.get("http://localhost:3000/")
+        await driver.manage().window().setRect(839, 864)
     })
     afterEach(async function() {
         await driver.quit();
@@ -17,19 +21,11 @@ describe('Header&FooterTesting', function () {
     it('HeaderLogoTesting', async function() {
         // Test name: Header&FooterTesting
         // Step # | name | target | value
-        // 1 | open | / |
-        await driver.get("http://localhost:3000/")
-        // 2 | setWindowSize | 755x864 |
-        await driver.manage().window().setRect(755, 864)
-        // 3 | click | id=Layer_1 |
+        // 1 | click | id=Layer_1 |
         await driver.findElement(By.id("Layer_1")).click()
     })
     it('HeaderDropDownMissionsTesting', async function() {
         // Step # | name | target | value
-        // 1 | open | / |
-        await driver.get("http://localhost:3000/")
-        // 2 | setWindowSize | 839x864 |
-        await driver.manage().window().setRect(839, 864)
         // 3 | click | css=.text-block |
         await driver.findElement(By.css(".text-block")).click()
         // 4 | click | linkText=Projects |
@@ -38,10 +34,6 @@ describe('Header&FooterTesting', function () {
     })
     it('HeaderDropDownEventsTesting', async function() {
         // Step # | name | target | value
-        // 1 | open | / |
-        await driver.get("http://localhost:3000/")
-        // 2 | setWindowSize | 839x864 |
-        await driver.manage().window().setRect(839, 864)
         // 3 | click | css=.text-block |
         await driver.findElement(By.css(".text-block")).click()
         // 4 | click | linkText=Projects |
@@ -50,10 +42,6 @@ describe('Header&FooterTesting', function () {
     })
     it('HeaderDropDownProjectTesting', async function() {
         // Step # | name | target | value
-        // 1 | open | / |
-        await driver.get("http://localhost:3000/")
-        // 2 | setWindowSize | 839x864 |
-        await driver.manage().window().setRect(839, 864)
         // 3 | click | css=.text-block |
         await driver.findElement(By.css(".text-block")).click()
         // 4 | click | linkText=Projects |
@@ -63,30 +51,18 @@ describe('Header&FooterTesting', function () {
     })
     it('HeaderContactTesting', async function() {
         // Step # | name | target | value
-        // 1 | open | / |
-        await driver.get("http://localhost:3000/")
-        // 2 | setWindowSize | 839x864 |
-        await driver.manage().window().setRect(839, 864)
         // 3 | click | css=.nav-link aka Contact |
         await driver.findElement(By.css(".nav-link")).click()
         await sleep(3000)
     })
     it('FooterPSUTesting', async function() {
         // Step # | name | target | value
-        // 1 | open | / |
-        await driver.get("http://localhost:3000/")
-        // 2 | setWindowSize | 839x864 |
-        await driver.manage().window().setRect(839, 864)
         // 3 | click | css=.nav-link aka Contact |
         await driver.findElement(By.linkText("Penn State Behrend")).click()
         await sleep(3000)
     })
     it('FooterASUTesting', async function() {
         // Step # | name | target | value
-        // 1 | open | / |
-        await driver.get("http://localhost:3000/")
-        // 2 | setWindowSize | 839x864 |
-        await driver.manage().window().setRect(839, 864)
         // 3 | click | css=.nav-link aka Contact |
         await driver.findElement(By.linkText("Arizona State University")).click()
         await sleep(3000)
@@ -94,13 +70,166 @@ describe('Header&FooterTesting', function () {
     it('FooterNASATesting', async function() {
         //Unable to access NASA Page due to InsecureCertificateError
         // Step # | name | target | value
-        // 1 | open | / |
-        await driver.get("http://localhost:3000/")
-        // 2 | setWindowSize | 839x864 |
-        await driver.manage().window().setRect(839, 864)
         // 3 | click | css=.nav-link aka Contact |
         await sleep(3000)
         await driver.findElement(By.linkText("NASA")).click()
         await sleep(3000)
     })
+    /*
+    * Logic Table:
+    * ---------------------------------------------------------
+    * | Electrode Keeper | Gas Feed  | Heat Insert | Result   |
+    * ---------------------------------------------------------
+    * |     0            |      0    |   0         | base     | done
+    * |     0            |      0    |   1         | heat     | done
+    * |     0            |      1    |   0         | gas      | done
+    * |     0            |      1    |   1         | internal | done
+    * |     1            |      0    |   0         | keeper   | done
+    * |     1            |      0    |   1         | k+h      | done
+    * |     1            |      1    |   0         | k+g      | done
+    * |     1            |      1    |   1         | external | done
+    * */
+    it('BaseDrawingTesting', async function(){
+        await sleep(3000)
+        await driver.findElement(By.id("LearningModeButton")).click()
+        await sleep(3000)
+    })
+
+    it('HeatInsertTesting', async function(){
+        await sleep(3000)
+        await driver.findElement(By.id("LearningModeButton")).click()
+        // activate heat insert scene
+        await driver.findElement(By.id("HeatInsertToggle")).click()
+        // deactivate heat insert scene
+        await driver.findElement(By.id("HeatInsertToggle")).click()
+        await sleep(3000)
+    })
+
+    it('GasFeedTesting', async function(){
+        await sleep(3000)
+        await driver.findElement(By.id("LearningModeButton")).click()
+        // activate gas feed scene
+        await driver.findElement(By.id("GasFeedToggle")).click()
+        // deactivate gas feed scene
+        await driver.findElement(By.id("GasFeedToggle")).click()
+        await sleep(3000)
+    })
+
+    it('InternalPlasmaTesting1', async function(){
+        await driver.get("http://localhost:3000/")
+        await driver.manage().window().setRect(839, 864)
+        await sleep(3000)
+        await driver.findElement(By.id("LearningModeButton")).click()
+        // activate heat insert scene
+        await driver.findElement(By.id("HeatInsertToggle")).click()
+        // activate gas feed scene
+        await driver.findElement(By.id("GasFeedToggle")).click()
+        await driver.findElement(By.id('nextButton')).click()
+        await sleep(3000)
+        // deactivate gas feed scene
+        await driver.findElement(By.id("GasFeedToggle")).click()
+    })
+
+    it('InternalPlasmaTesting2', async function(){
+        await driver.findElement(By.id("LearningModeButton")).click()
+        // activate heat insert scene
+        await driver.findElement(By.id("HeatInsertToggle")).click()
+        // activate gas feed scene
+        await driver.findElement(By.id("GasFeedToggle")).click()
+        await driver.findElement(By.id('nextButton')).click()
+        await sleep(3000)
+        //deactivate heat insert scene
+        await driver.findElement(By.id("HeatInsertToggle")).click()
+    })
+
+    it('ElectrodeKeeperTesting', async function(){
+        await sleep(3000)
+        await driver.findElement(By.id("LearningModeButton")).click()
+        // activate keeper electrode scene
+        await driver.findElement(By.id("KeeperElectrodeToggle")).click()
+        // deactivate keeper electrode  scene
+        await driver.findElement(By.id("KeeperElectrodeToggle")).click()
+        await sleep(3000)
+    })
+
+    it('Keeper&HeaterTesting1', async function(){
+        await driver.manage().window().setRect(839, 864)
+        await sleep(3000)
+        await driver.findElement(By.id("LearningModeButton")).click()
+        // activate keeper electrode scene
+        await driver.findElement(By.id("KeeperElectrodeToggle")).click()
+        // activate heat insert  scene
+        await driver.findElement(By.id("HeatInsertToggle")).click()
+        await sleep(3000)
+    })
+
+    it('Keeper&HeaterTesting2', async function(){
+        await sleep(3000)
+        await driver.findElement(By.id("LearningModeButton")).click()
+        // activate heat insert  scene
+        await driver.findElement(By.id("HeatInsertToggle")).click()
+        // activate keeper electrode scene
+        await driver.findElement(By.id("KeeperElectrodeToggle")).click()
+        await sleep(3000)
+    })
+
+    it('Keeper&GasTesting1', async function(){
+        await sleep(3000)
+        await driver.findElement(By.id("LearningModeButton")).click()
+        // activate keeper electrode scene
+        await driver.findElement(By.id("KeeperElectrodeToggle")).click()
+        // activate gas feed  scene
+        await driver.findElement(By.id("GasFeedToggle")).click()
+        await sleep(3000)
+    })
+
+    it('Keeper&GasTesting2', async function(){
+        await sleep(3000)
+        await driver.findElement(By.id("LearningModeButton")).click()
+        // activate gas feed  scene
+        await driver.findElement(By.id("GasFeedToggle")).click()
+        // activate keeper electrode scene
+        await driver.findElement(By.id("KeeperElectrodeToggle")).click()
+        await sleep(3000)
+    })
+
+    it('ExternalPlasmaPathTesting', async function(){
+        let idList = ["HeatInsertToggle", "GasFeedToggle", "KeeperElectrodeToggle"]
+        await driver.findElement(By.id("LearningModeButton")).click()
+        await permute(idList, 0, idList.length-1)
+        await sleep(5000)
+    })
+
+    async function permute(list, l, r){
+        if(l==r){
+            console.log(list+"\n")
+            let i=0;
+            while(i<3){
+                let elements= await driver.findElements(By.id('nextButton'))
+                if(elements!=0){
+                    await driver.findElement(By.id('nextButton')).click()
+                    await sleep(2000)
+                }
+                await driver.findElement(By.id(list[i++])).click()
+                await sleep(3000)
+            }
+            await reset()
+        }else {
+            for (let i = l; i <= r; i++) {
+                swap(list, l, i)
+                await permute(list, l + 1, r)
+                swap(list, l, i)
+            }
+        }
+    }
+    function swap(list, l, r){
+        let tmp= list[l]
+        list[l]= list[r];
+        list[r]= tmp;
+    }
+    async function reset(){
+        await driver.get("http://localhost:3000/")
+        await driver.manage().window().setRect(839, 864)
+        await driver.findElement(By.id("LearningModeButton")).click()
+    }
 });
