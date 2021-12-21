432f0ad167a51a2c091d1d7536637dc8ec615a0c
HuyTran <htt5059@psu.edu>
2021-12-21 11:03:05 -0600

    added assert function and set up applitools

--- src/components/LearningMode/learn.test.js
+++ src/components/LearningMode/learn.test.js
@@ -1,22 +1,30 @@
 const should = require('chai').should()
 const {promisify} = require('util')
 const {Builder, By, Key} = require("selenium-webdriver")
+const { Eyes, VisualGridRunner, Target, RectangleSize, Configuration, BatchInfo, BrowserType, DeviceName, ScreenOrientation} = require('@applitools/eyes-selenium');
 const sleep= promisify(setTimeout)
-//const {Eyes, Target, Configuration, BatchInfo} = require('@applitools/eyes-images')
 //const {config} = require("chai");
 
 describe('LearningModeTesting', function () {
     this.timeout(300000)
     let driver
-    let vars
+    let eyes, runner
     beforeEach(async function () {
         driver = await new Builder().forBrowser('firefox').build()
-        vars = {}
+        runner = new VisualGridRunner(1)
+        eyes = new Eyes(runner)
+        let config = new Configuration()
+        config.setBatch(new BatchInfo("Hollow Cathode Learning Batch"))
+        config.addBrowser(839, 864, BrowserType.CHROME);
+        config.setApiKey("LyBNg7PUAI2LrhPJLJBt3P0jNjupYxn0CXaZzfpIJhc110")
+        eyes.setConfiguration(config)
         await driver.get("http://localhost:3000/")
-        await driver.manage().window().setRect(839, 864)
     })
     afterEach(async function () {
         await driver.quit();
+        await eyes.abortAsync();
+        const allTestResults= await runner.getAllTestResults()
+        console.log(allTestResults)
     })
     /*
     * Logic Table:
@@ -34,32 +42,41 @@ describe('LearningModeTesting', function () {
     * */
     it('BaseDrawingTesting', async function(){
         await sleep(1000)
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'BaseDrawing demo', new RectangleSize(839, 864));
+        await eyes.check("BasDrawing Window", Target.window().fully());
         await driver.findElement(By.id("canvasHolder")).click()
         await sleep(2000)
+        await eyes.close()
     })
 
     it('HeatInsertTesting', async function(){
         await sleep(1000)
-        await driver.findElement(By.id("canvasHolder")).click()
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'HeatInsert demo', new RectangleSize(839, 864));
+        await driver.findElement(By.id("canvasHolder")).click();
         // activate heat insert scene
-        await driver.findElement(By.id("HeatInsertToggle")).click()
+        await driver.findElement(By.id("HeatInsertToggle")).click();
+        await eyes.check("Heat Insert Window", Target.window().fully());
         // deactivate heat insert scene
         await driver.findElement(By.id("HeatInsertToggle")).click()
         await sleep(2000)
+        await eyes.close()
     })
 
     it('GasFeedTesting', async function(){
         await sleep(1000)
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'GasFeed demo', new RectangleSize(839, 864));
         await driver.findElement(By.id("canvasHolder")).click()
         // activate gas feed scene
         await driver.findElement(By.id("GasFeedToggle")).click()
+        await eyes.check("Heat Insert Window", Target.window().fully());
         // deactivate gas feed scene
         await driver.findElement(By.id("GasFeedToggle")).click()
         await sleep(2000)
+        await eyes.close()
     })
 
     it('InternalPlasmaTesting1', async function(){
-        await driver.get("http://localhost:3000/")
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'InternalPlasma1 demo', new RectangleSize(839, 864));
         await driver.manage().window().setRect(839, 864)
         await sleep(1000)
         await driver.findElement(By.id("canvasHolder")).click()
@@ -68,12 +85,15 @@ describe('LearningModeTesting', function () {
         // activate gas feed scene
         await driver.findElement(By.id("GasFeedToggle")).click()
         await driver.findElement(By.id('nextButton')).click()
+        await eyes.check("Internal Plasma Window 1", Target.window().fully());
         await sleep(2000)
         // deactivate gas feed scene
         await driver.findElement(By.id("GasFeedToggle")).click()
+        await eyes.close()
     })
 
     it('InternalPlasmaTesting2', async function(){
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'InternalPlasma2 demo', new RectangleSize(839, 864));
         await driver.findElement(By.id("canvasHolder")).click()
         // activate heat insert scene
         await driver.findElement(By.id("HeatInsertToggle")).click()
@@ -81,86 +101,106 @@ describe('LearningModeTesting', function () {
         await driver.findElement(By.id("GasFeedToggle")).click()
         await driver.findElement(By.id('nextButton')).click()
         await sleep(1000)
+        await eyes.check("Heat Insert Window 2", Target.window().fully());
         //deactivate heat insert scene
         await driver.findElement(By.id("HeatInsertToggle")).click()
+        await eyes.close()
     })
 
     it('ElectrodeKeeperTesting', async function(){
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'ElectrodeKeeper demo', new RectangleSize(839, 864));
         await sleep(1000)
         await driver.findElement(By.id("canvasHolder")).click()
         // activate keeper electrode scene
         await driver.findElement(By.id("KeeperElectrodeToggle")).click()
+        await eyes.check("ElectrodeKeeper Window", Target.window().fully());
         // deactivate keeper electrode  scene
         await driver.findElement(By.id("KeeperElectrodeToggle")).click()
         await sleep(1000)
+        await eyes.close()
     })
 
     it('Keeper&HeaterTesting1', async function(){
-        await driver.manage().window().setRect(839, 864)
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'Keeper&Heater1 demo', new RectangleSize(839, 864));
         await sleep(1000)
         await driver.findElement(By.id("canvasHolder")).click()
         // activate keeper electrode scene
         await driver.findElement(By.id("KeeperElectrodeToggle")).click()
         // activate heat insert  scene
         await driver.findElement(By.id("HeatInsertToggle")).click()
+        await eyes.check("Keeper&Heater1 Window", Target.window().fully());
         await sleep(1000)
+        await eyes.close()
     })
 
     it('Keeper&HeaterTesting2', async function(){
         await sleep(1000)
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'Keeper&Heater2 demo', new RectangleSize(839, 864));
         await driver.findElement(By.id("canvasHolder")).click()
         // activate heat insert  scene
         await driver.findElement(By.id("HeatInsertToggle")).click()
         // activate keeper electrode scene
         await driver.findElement(By.id("KeeperElectrodeToggle")).click()
+        await eyes.check("Keeper&Heater2 Window", Target.window().fully());
         await sleep(1000)
+        await eyes.close()
     })
 
     it('Keeper&GasTesting1', async function(){
         await sleep(1000)
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'Keeper&Gas1 demo', new RectangleSize(839, 864));
         await driver.findElement(By.id("canvasHolder")).click()
         // activate keeper electrode scene
         await driver.findElement(By.id("KeeperElectrodeToggle")).click()
         // activate gas feed  scene
         await driver.findElement(By.id("GasFeedToggle")).click()
+        await eyes.check("Keeper&Gas1 Window", Target.window().fully());
         await sleep(1000)
+        await eyes.close()
     })
 
     it('Keeper&GasTesting2', async function(){
         await sleep(1000)
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'Keeper&Gas2 demo', new RectangleSize(839, 864));
         await driver.findElement(By.id("canvasHolder")).click()
         // activate gas feed  scene
         await driver.findElement(By.id("GasFeedToggle")).click()
         // activate keeper electrode scene
         await driver.findElement(By.id("KeeperElectrodeToggle")).click()
+        await eyes.check("Keeper&Gas2 Window", Target.window().fully());
         await sleep(1000)
+        await eyes.close()
     })
 
     it('ExternalPlasmaPathTesting', async function(){
         let idList = ["HeatInsertToggle", "GasFeedToggle", "KeeperElectrodeToggle"]
+        await eyes.open(driver, 'Hollow Cathode Visualization App', 'IntegrateTesting demo', new RectangleSize(839, 864));
         await driver.findElement(By.id("canvasHolder")).click()
-        await permute(idList, 0, idList.length-1)
+        await sleep(3000)
+        await permute(idList, 0, idList.length-1, 0)
         await sleep(5000)
     })
 
-    async function permute(list, l, r){
+    async function permute(list, l, r, times){
         if(l==r){
-            console.log(list+"\n")
+            console.log(list+"\n"+times)
             let i=0
             let elements
             while(i<3){
+                await eyes.check(list[i], Target.window().fully());
                 await driver.findElement(By.id(list[i++])).click()
                 elements= await driver.findElements(By.id('nextButton'))
                 if(elements.length!=0){
+                    await eyes.check('nextButton', Target.window().fully());
                     await driver.findElement(By.id('nextButton')).click()
                 }
                 await sleep(500)
             }
-            await reset()
+            await reset(times)
         }else {
             for (let i = l; i <= r; i++) {
                 swap(list, l, i)
-                await permute(list, l + 1, r)
+                await permute(list, l + 1, r, ++times)
                 swap(list, l, i)
             }
         }
@@ -170,8 +210,9 @@ describe('LearningModeTesting', function () {
         list[l]= list[r];
         list[r]= tmp;
     }
-    async function reset(){
+    async function reset(times){
         await driver.findElement(By.id('backButton')).click()
         await driver.findElement(By.id("canvasHolder")).click()
+        await eyes.check("Reset Window "+times, Target.window().fully());
     }
 });
--- src/components/menu/menu.test.js
+++ src/components/menu/menu.test.js
@@ -1,21 +1,21 @@
-const should = require('chai').should()
 const {promisify} = require('util')
 const {Builder, By, Key} = require("selenium-webdriver")
-const sleep= promisify(setTimeout)
+const sleep = promisify(setTimeout)
 
 //command running the test case: npx mocha 'menu.test.js'
 
 describe('Header&FooterTesting', function () {
     this.timeout(300000)
     let driver
-    let vars
+    let assert
     beforeEach(async function() {
         driver = await new Builder().forBrowser('chrome').build()
-        vars = {}
+        assert= require('chai').assert
         await driver.get("http://localhost:3000/")
         await driver.manage().window().setRect(839, 864)
     })
     afterEach(async function() {
+        await sleep(1000)
         await driver.quit();
     })
     it('HeaderLogoTesting', async function() {
@@ -23,6 +23,8 @@ describe('Header&FooterTesting', function () {
         // Step # | name | target | value
         // 1 | click | id=Layer_1 |
         await driver.findElement(By.css(".brand > img:nth-child(1)")).click()
+        let currentURL = await driver.getCurrentUrl()
+        await assert(currentURL === "https://psyche.asu.edu/", "Not PSYCHE Page!!!")
     })
     it('HeaderDropDownMissionsTesting', async function() {
         // Step # | name | target | value
@@ -31,7 +33,7 @@ describe('Header&FooterTesting', function () {
         await driver.findElement(By.css(".text-block")).click()
         // 4 | click | linkText=Projects |
         await driver.findElement(By.linkText("Missions")).click()
-        await sleep(1000)
+        await assert(await driver.getCurrentUrl() === "https://psyche.asu.edu/mission/", "Not MISSION Page!!!")
     })
     it('HeaderDropDownEventsTesting', async function() {
         // Step # | name | target | value
@@ -39,7 +41,7 @@ describe('Header&FooterTesting', function () {
         await driver.findElement(By.css(".text-block")).click()
         // 4 | click | linkText=Projects |
         await driver.findElement(By.linkText("Events")).click()
-        await sleep(2000)
+        await assert(await driver.getCurrentUrl() === "https://psyche.asu.edu/events/", "Not EVENTS Page!!!")
     })
     it('HeaderDropDownProjectTesting', async function() {
         // Step # | name | target | value
@@ -48,25 +50,26 @@ describe('Header&FooterTesting', function () {
         // 4 | click | linkText=Projects |
         await sleep(2000)
         await driver.findElement(By.linkText("Projects")).click()
-        await sleep(2000)
+        const expected ="https://psyche.asu.edu/get-involved/capstone-projects/capstone-projects-copper-class/hollow-cathode-visualization-penn-state-behrend/"
+        await assert(await driver.getCurrentUrl() === expected, "Not EVENTS Page!!!")
     })
     it('HeaderContactTesting', async function() {
         // Step # | name | target | value
         // 3 | click | css=.nav-link aka Contact |
         await driver.findElement(By.css(".nav-link")).click()
-        await sleep(2000)
+        await assert(await driver.getCurrentUrl() === "https://psyche.asu.edu/contact/", "Not CONTACT Page!!!")
     })
     it('FooterPSUTesting', async function() {
         // Step # | name | target | value
         // 3 | click | css=.nav-link aka Contact |
         await driver.findElement(By.linkText("Penn State Behrend")).click()
-        await sleep(2000)
+        await assert(await driver.getCurrentUrl() === "https://behrend.psu.edu/", "Not PSBehrend Page!!!")
     })
     it('FooterASUTesting', async function() {
         // Step # | name | target | value
         // 3 | click | css=.nav-link aka Contact |
         await driver.findElement(By.linkText("Arizona State University")).click()
-        await sleep(2000)
+        await assert(await driver.getCurrentUrl() === "https://www.asu.edu/", "Not ASU Page!!!")
     })
     it('FooterNASATesting', async function() {
         // Unable to access NASA Page due to InsecureCertificateError
@@ -74,6 +77,6 @@ describe('Header&FooterTesting', function () {
         // 3 | click | css=.nav-link aka Contact |
         await sleep(2000)
         await driver.findElement(By.linkText("NASA")).click()
-        await sleep(2000)
+        await assert(await driver.getCurrentUrl() === "https://www.nasa.gov/", "Not NASA Page!!!")
     })
 });
