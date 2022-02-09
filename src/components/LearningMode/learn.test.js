3b46c86369647ba156b7b6f320c5f333b9a59b0d
HuyTran <htt5059@psu.edu>
2022-02-09 17:51:55 -0500

    update the learning test file

--- src/components/LearningMode/learn.test.js
+++ src/components/LearningMode/learn.test.js
@@ -18,6 +18,21 @@ describe('LearningModeTesting', function () {
     afterEach(async function () {
         await driver.quit();
     })
+
+
+    it('HallThrusterOffIntro', async function(){
+        await sleep(3000)
+        await driver.findElement(By.id("canvasHolder")).click()
+        await sleep(3000)
+    })
+
+    it('HallThrusterOnIntro', async function(){
+        await sleep(3000)
+        await driver.findElement(By.id("canvasHolder")).click()
+        await sleep(3000)
+        await driver.findElement(By.id('HallThrusterToggle')).click()
+        await sleep(3000)
+    })
     /*
     * Logic Table:
     * ---------------------------------------------------------
@@ -33,14 +48,11 @@ describe('LearningModeTesting', function () {
     * |     1            |      1    |   1         | external | done
     * */
     it('BaseDrawingTesting', async function(){
-        await sleep(3000)
-        await driver.findElement(By.id("canvasHolder")).click()
-        await sleep(3000)
+        await toBaseDrawing()
     })
 
     it('HeatInsertTesting', async function(){
-        await sleep(3000)
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         // activate heat insert scene
         await driver.findElement(By.id("HeatInsertToggle")).click()
         // deactivate heat insert scene
@@ -49,8 +61,7 @@ describe('LearningModeTesting', function () {
     })
 
     it('GasFeedTesting', async function(){
-        await sleep(3000)
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         // activate gas feed scene
         await driver.findElement(By.id("GasFeedToggle")).click()
         // deactivate gas feed scene
@@ -59,10 +70,7 @@ describe('LearningModeTesting', function () {
     })
 
     it('InternalPlasmaTesting1', async function(){
-        await driver.get("http://localhost:3000/")
-        await driver.manage().window().setRect(839, 864)
-        await sleep(3000)
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         // activate heat insert scene
         await driver.findElement(By.id("HeatInsertToggle")).click()
         // activate gas feed scene
@@ -74,7 +82,7 @@ describe('LearningModeTesting', function () {
     })
 
     it('InternalPlasmaTesting2', async function(){
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         // activate heat insert scene
         await driver.findElement(By.id("HeatInsertToggle")).click()
         // activate gas feed scene
@@ -86,8 +94,7 @@ describe('LearningModeTesting', function () {
     })
 
     it('ElectrodeKeeperTesting', async function(){
-        await sleep(3000)
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         // activate keeper electrode scene
         await driver.findElement(By.id("KeeperElectrodeToggle")).click()
         // deactivate keeper electrode  scene
@@ -97,8 +104,7 @@ describe('LearningModeTesting', function () {
 
     it('Keeper&HeaterTesting1', async function(){
         await driver.manage().window().setRect(839, 864)
-        await sleep(3000)
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         // activate keeper electrode scene
         await driver.findElement(By.id("KeeperElectrodeToggle")).click()
         // activate heat insert  scene
@@ -107,8 +113,7 @@ describe('LearningModeTesting', function () {
     })
 
     it('Keeper&HeaterTesting2', async function(){
-        await sleep(3000)
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         // activate heat insert  scene
         await driver.findElement(By.id("HeatInsertToggle")).click()
         // activate keeper electrode scene
@@ -117,8 +122,7 @@ describe('LearningModeTesting', function () {
     })
 
     it('Keeper&GasTesting1', async function(){
-        await sleep(3000)
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         // activate keeper electrode scene
         await driver.findElement(By.id("KeeperElectrodeToggle")).click()
         // activate gas feed  scene
@@ -127,8 +131,7 @@ describe('LearningModeTesting', function () {
     })
 
     it('Keeper&GasTesting2', async function(){
-        await sleep(3000)
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         // activate gas feed  scene
         await driver.findElement(By.id("GasFeedToggle")).click()
         // activate keeper electrode scene
@@ -138,10 +141,18 @@ describe('LearningModeTesting', function () {
 
     it('ExternalPlasmaPathTesting', async function(){
         let idList = ["HeatInsertToggle", "GasFeedToggle", "KeeperElectrodeToggle"]
-        await driver.findElement(By.id("canvasHolder")).click()
+        await toBaseDrawing()
         await permute(idList, 0, idList.length-1)
         await sleep(5000)
     })
+
+    async function toBaseDrawing(){
+        await sleep(3000)
+        await driver.findElement(By.id('canvasHolder')).click()
+        await sleep(1000)
+        await driver.findElement(By.id('HallThrusterNext')).click()
+        await sleep(3000)
+    }
 
     async function permute(list, l, r){
         if(l==r){
@@ -149,9 +160,14 @@ describe('LearningModeTesting', function () {
             let i=0
             let elements
             while(i<3){
-                await driver.findElement(By.id(list[i++])).click()
+                //await eyes.check(list[i], Target.window().fully());
+                await driver.findElement(By.id(list[i])).click()
+                console.log(list[i++])
+                await sleep(1000)
                 elements= await driver.findElements(By.id('nextButton'))
-                if(elements.length!=0){
+                console.log(elements.length)
+                if(elements.length==1){
+                    //await eyes.check('nextButton', Target.window().fully());
                     await driver.findElement(By.id('nextButton')).click()
                 }
                 await sleep(500)
