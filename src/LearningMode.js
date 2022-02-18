16b3f147cea1c3815395734353cf63f59b32866f
JackBlicha <57300971+JackBlicha@users.noreply.github.com>
2022-02-18 13:06:45 -0500

    Added images from artist to particles and background

--- src/LearningMode.js
+++ src/LearningMode.js
@@ -258,7 +258,6 @@ export class LearningMode extends React.Component {
         else if (this.state.deltastage === plasma){
             // the user deselected this option/layer
             this.painter.clearCanvas(this.state.deltastage);
-            this.painter.plasma = false; // no plasma (flag)
 
             // if internal plasma stops because ___ call ___ explanation
             if(!this.state.scene[heat]){
