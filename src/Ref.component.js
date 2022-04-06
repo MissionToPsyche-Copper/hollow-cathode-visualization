50c939c1179e18bcfea883c86c73c9078d829701
HuyTran <htt5059@psu.edu>
2022-04-05 20:46:20 -0400

    fixed references and links

--- src/Ref.component.js
+++ src/Ref.component.js
@@ -17,22 +17,27 @@ class RefComponent extends React.Component{
             </div>
             <div className={'summarySubLabel summarySubLabelPos'}>
                 <label>
-                [1]    D. M. Goebel and I. Katz, “Fundamentals of Electric Propulsion: Ion and Hall Thrusters,” JPL Space Science & Technology Series, pp. 3–6, Mar. 2008.
+                    [1]    D. M. Goebel and I. Katz, “Fundamentals of Electric Propulsion: Ion and Hall Thrusters,” <i>JPL Space Science & Technology Series</i>, chapter 1-2, Mar. 2008. Available: <a href={"https://descanso.jpl.nasa.gov/SciTechBook/series1/Goebel__cmprsd_opt.pdf"} className={'reference-link'}>https://descanso.jpl.nasa.gov/SciTechBook/series1/Goebel__cmprsd_opt.pdf.</a> [Accessed: 09-Oct-2021].
                 </label>
-                <br></br>
-                <br></br>
+                <br/>
+                <br/>
                 <label>
-                [2]    “Glossary,” Glossary | MIT Plasma Science and Fusion Center, 2021. [Online]. Available: <a href={"https://www.psfc.mit.edu/vision/glossary."}>https://www.psfc.mit.edu/vision/glossary.</a> [Accessed: 09-Oct-2021].
+                    [2]    D. M. Goebel and I. Katz, “Fundamentals of Electric Propulsion: Ion and Hall Thrusters,” <i>JPL Space Science & Technology Series</i>, chapter 6, Mar. 2008. Available: <a href={"https://descanso.jpl.nasa.gov/SciTechBook/series1/Goebel__cmprsd_opt.pdf"} className={'reference-link'}>https://descanso.jpl.nasa.gov/SciTechBook/series1/Goebel__cmprsd_opt.pdf.</a> [Accessed: 09-Oct-2021].
                 </label>
-                <br></br>
-                <br></br>
+                <br/>
+                <br/>
                 <label>
-                [3]    J. D. Frieman, “CHARACTERIZATION OF BACKGROUND NEUTRAL FLOWS IN VACUUM TEST FACILITIES AND IMPACTS ON HALL EFFECT THRUSTER OPERATION,” dissertation, Georgia Institute of Technology, Atlanta, GA, 2017.
+                    [3]    “Glossary,” Glossary | MIT Plasma Science and Fusion Center, 2021. [Online]. Available: <a href={"https://www.psfc.mit.edu/vision/glossary."} className={'reference-link'}>https://www.psfc.mit.edu/vision/glossary.</a> [Accessed: 09-Oct-2021].
                 </label>
-                <br></br>
+                <br/>
+                <br/>
+                <label>
+                    [4]    J. D. Frieman, “CHARACTERIZATION OF BACKGROUND NEUTRAL FLOWS IN VACUUM TEST FACILITIES AND IMPACTS ON HALL EFFECT THRUSTER OPERATION,” dissertation, Georgia Institute of Technology, Atlanta, GA, 2017. Available: <a href={"https://mwalker.gatech.edu/papers/FRIEMAN-DISSERTATION-2017.pdf"} className={'reference-link'}>https://mwalker.gatech.edu/papers/FRIEMAN-DISSERTATION-2017.pdf.</a> [Accessed: 09-Oct-2021].
+                </label>
+                <br/>
                 <br/>
                 <label style={{"margin-top":"1rem"}}>
-                [4]    S. T. Lai and K. Cahoy, “Spacecraft Charging,” Encyclopedia of Plasma Technology, pp. 1352–1366, Dec. 2016.
+                    [5]    S. T. Lai and K. Cahoy, “Spacecraft Charging,” Encyclopedia of Plasma Technology, pp. 1352–1366, Dec. 2016. Available: <a href={"https://www.bc.edu/content/dam/files/research_sites/isr/pdf/2017%20Lai%20%26%20Cahoy%20-%20Encyclo.pdf"} className={'reference-link'}>https://www.bc.edu/content/dam/files/research_sites/isr/pdf/2017%20Lai%20%26%20Cahoy%20-%20Encyclo.pdf.</a> [Accessed: 09-Oct-2021].
                 </label>
             </div>
         </div>
