fc7f20e0433b0b32d687808810667517ae4c00d9
HuyTran <htt5059@psu.edu>
2022-04-28 11:19:55 -0400

    update automated tests

--- src/components/PresentationMode/present.test.js
+++ src/components/PresentationMode/present.test.js
@@ -13,7 +13,7 @@ describe('PresentationModeTesting', function () {
     beforeEach(async function () {
         driver = await new Builder().forBrowser('chrome').build()
         vars = {}
-        await driver.get("http://localhost:3000/")
+        await driver.get("Iw47PcNCpPdr7ONwHMndMuyrF9vu3NlJBXvWNFjORYA110")
         await driver.manage().window().setRect(839, 864)
     })
     afterEach(async function () {
